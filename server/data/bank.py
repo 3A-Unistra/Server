4aa0996ea279647e9ce45cd6e65d953cbc649a67
SchawnnDev <iperskill@gmail.com>
2022-02-18 20:29:06 +0100

    finished to impl states like UML schema

--- server/data/bank.py
+++ server/data/bank.py
@@ -1,3 +1,6 @@
 class Bank:
+    nb_house: int
+    nb_hotel: int
+
     def __init__(self):
         pass
--- server/data/card.py
+++ server/data/card.py
@@ -1,3 +1,17 @@
+from enum import Enum
+
+
+class CardType(Enum):
+    Community = 0
+    Chance = 1
+
+
 class Card:
-    def __init__(self):
-        pass
+    id_: int
+    card_type: CardType
+    description: str
+
+    def __init__(self, id_: int, card_type: CardType, description: str):
+        self.id_ = id_
+        self.card_type = card_type
+        self.description = description
