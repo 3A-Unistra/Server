d5d439550a8b0d1e49fb97df89407de11e633ae4
Aki Schmatzler <aki.schmatzler@gmail.com>
2022-02-22 13:56:09 +0100

    starting

--- server/data/packets/packets_def.py
+++ server/data/packets/packets_def.py
@@ -1,3 +1,27 @@
 """
 TODO : définir tout les paquets
+
+ Créer un constructeur vide
+
+
+ Créer un constructeur avec tous les arguments possibles pour chaque paquet
+
+
+ Bien typer chaque variable
+
+
+Implémenter les fonctions serialize/deserialize pour tout les paquets
 """
+
+import json
+
+
+class AppletPrepare(Packet) :
+    def __init__(self, name : str):
+        super(AppletPrepare, self).__init__(name)
+
+    def serialize(self) -> str:
+        json.dumps(self.__dict__, skipkeys = True)
+
+    def deserialize(self, obj: object) -> "AppletPrepare":
+        pass
